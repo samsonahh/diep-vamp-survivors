c783ac5a4c9c2801dea1595ab70dd657818c8e20
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2024-09-04 16:45:25 -0400

    first game p1

---
+++ Assets/FirstGame/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+namespace FirstGame
+{
+    public class GameManager : MonoBehaviour
+    {
+        
+    }
+}
+
